7aa779e6d0ff14d15a484af2baeda8e4023a37cb
AlinaKuzmenko <k.alina.2014@ukr.net>
2016-05-24 12:24:24 +0300

    delete hw-js-3-4

--- HW-JS-7-8/js/task2.js
+++ HW-JS-7-8/js/task2.js
@@ -1,14 +1,13 @@
 $(function () {
     $('.input-label')
         .mouseenter(function() {
-            $('.help-text').fadeOut(100);
             $(this).children('.help-text').fadeIn(100);
         })
         .mouseleave(function () {
-            $('.help-text').fadeOut(100);
+            $(this).children('.help-text').fadeOut(100);
         });
 
     $('.show-help-texts').click(function () {
-        $('.help-text').css('display', 'inline');
+        $('.help-text').fadeIn(100);
     });
 });
