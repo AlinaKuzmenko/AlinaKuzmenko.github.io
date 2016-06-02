98761418023ee8f336934aca905c8ed3b867a3af
AlinaKuzmenko <k.alina.2014@ukr.net>
2016-06-02 13:07:06 +0300

    add a slide-down menu

--- HW-JS-9-10/js/script.js
+++ HW-JS-9-10/js/script.js
@@ -1,5 +1,55 @@
 (function($) {
     $(function() {
+        // SLIDE-DOWN MENU
+        $('.sub-list').hide();
+        $('.sub-sub-list').hide();
+
+        $('.list-item')
+            .hover(
+                function() {
+                    $(this).children('.sub-list')
+                        .animate({
+                            top: '25px',
+                            left: '0',
+                            backgroundColor: '#3a3a3a'
+                        }, 0)
+                        .fadeIn(200)
+
+                }, function () {
+                    $(this).children('.sub-list').hide(200);
+                });
+
+        $('.sub-list-item')
+            .css('position', 'relative')
+            .hover(
+                function() {
+                    $(this).children('.sub-sub-list')
+                        .animate({
+                            top: '0',
+                            left: '120px',
+                            backgroundColor: '#555'
+                        }, 0)
+                        .fadeIn(200)
+                }, function () {
+                    $(this).children('.sub-sub-list').fadeOut(200);
+                }
+            );
+
+
+        // SELECTS
+        $('select').selectBox({
+            mobile: true,
+            menuTransition: 'fade',
+            menuSpeed: 'normal',
+            loopOptions: true
+        });
+
+        $('select').on('click', function() {
+            $(this).children('option').addClass('.custom-option');
+        });
+
+        // JCAROUSEL
+
         $('.jcarousel').jcarousel({
             wrap: 'circular',
             scroll: '+=1'
@@ -35,17 +85,5 @@
                 $(this).removeClass('active');
             })
             .jcarouselPagination();
-
-        $('select').selectBox({
-            mobile: true,
-            menuTransition: 'fade',
-            menuSpeed: 'normal',
-            loopOptions: true
-        });
-
-        $('select').on('click', function() {
-            $(this).children('option').addClass('.custom-option');
-        });
-
     });
 })(jQuery);
