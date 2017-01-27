592860ea3038c7413fca1d416b0d2e7e2296b635
AlinaKuzmenko <k.alina.2014@ukr.net>
2017-01-27 19:34:26 +0200

    toggle nav links while scrolling

--- SportLabsGroup/js/app.js
+++ SportLabsGroup/js/app.js
@@ -12,8 +12,35 @@ var changeHeaderColor = function changeHeaderColor() {
         header.classList.remove('smaller');
         navTop.classList.remove('smaller');
     }
-};
+};;"use strict";
 
 window.onscroll = function () {
     changeHeaderColor();
+    toggleLinkClass();
+};;'use strict';
+
+var setClasses = function setClasses(links, activeLink) {
+    links.forEach(function (e) {
+        e.classList.remove('active');
+    });
+    activeLink.classList.add('active');
 };
+
+var toggleLinkClass = function toggleLinkClass() {
+    var scroll = window.pageYOffset || document.documentElement.scrollTop;
+    var navTopLinksCollection = document.getElementsByClassName('nav-top--link');
+    var navTopLinks = Array.prototype.slice.call(navTopLinksCollection);
+
+    if (scroll <= 737) {
+        setClasses(navTopLinks, navTopLinks[0]);
+    }
+    if (scroll >= 738 && scroll <= 1482) {
+        setClasses(navTopLinks, navTopLinks[1]);
+    }
+    if (scroll >= 1483 && scroll < 1920) {
+        setClasses(navTopLinks, navTopLinks[2]);
+    }
+    if (scroll >= 1921) {
+        setClasses(navTopLinks, navTopLinks[3]);
+    }
+};;"use strict";
