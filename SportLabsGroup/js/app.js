684a6e3f810bea5774b4e03ef8bcf003b1001159
AlinaKuzmenko <k.alina.2014@ukr.net>
2017-01-30 17:32:10 +0200

    change map size

--- SportLabsGroup/js/app.js
+++ SportLabsGroup/js/app.js
@@ -51,13 +51,32 @@ var toggleLinksOnClick = function toggleLinksOnClick(navTopLinks) {
             var href = link.target.getAttribute('href');
             var relatedLinks = Array.prototype.slice.call(document.querySelectorAll('a[href=\'' + href + '\']'));
             var sectionId = href.slice(1);
-            var section = sectionId !== '' ? document.getElementById(sectionId) : document.getElementById('header-top');
+            var section = sectionId !== '' ? document.getElementById(sectionId) : document.getElementById('header');
             var scrollFrom = window.pageYOffset;
             var scrollTo = section.offsetTop - 60;
             setClasses(navTopLinks, relatedLinks[0], relatedLinks[1]);
             animate(document.scrollingElement || document.documentElement, "scrollTop", "", scrollFrom, scrollTo, 700, true);
         });
     });
+};
+
+var toggleLinkClass = function toggleLinkClass(scroll, navTopLinks) {
+    var aboutOffset = document.getElementById('about').offsetTop - 60;
+    var projectsOffset = document.getElementById('projects').offsetTop - 60;
+    var contactsOffset = document.getElementById('contacts').offsetTop - 60;
+
+    if (scroll <= aboutOffset - 1) {
+        setClasses(navTopLinks, navTopLinks[0], navTopLinks[4]);
+    }
+    if (scroll >= aboutOffset && scroll <= projectsOffset - 1) {
+        setClasses(navTopLinks, navTopLinks[1], navTopLinks[5]);
+    }
+    if (scroll >= projectsOffset && scroll < contactsOffset - 1) {
+        setClasses(navTopLinks, navTopLinks[2], navTopLinks[6]);
+    }
+    if (scroll >= contactsOffset) {
+        setClasses(navTopLinks, navTopLinks[3], navTopLinks[7]);
+    }
 };;'use strict';
 
 function initMap() {
@@ -75,10 +94,11 @@ function initMap() {
 
 var navTopLinks = void 0;
 
+var getNavTopLinks = function getNavTopLinks() {
+    return navTopLinks = Array.prototype.slice.call(document.getElementsByClassName('nav-link'));
+};
+
 window.onload = function () {
-    var getNavTopLinks = function getNavTopLinks() {
-        return navTopLinks = Array.prototype.slice.call(document.getElementsByClassName('nav-link'));
-    };
     getNavTopLinks();
     toggleLinksOnClick(navTopLinks);
 };
@@ -86,4 +106,6 @@ window.onload = function () {
 window.onscroll = function () {
     var scroll = window.pageYOffset || document.documentElement.scrollTop;
     changeHeaderColor(scroll);
+    getNavTopLinks();
+    toggleLinkClass(scroll, navTopLinks);
 };
